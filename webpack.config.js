e0f68bc6c2d8d51992dc491b1829f952a884b55a
Ibrahim <flamexshady@gmail.com>
2021-04-02 16:20:18 -0500

    Base Repo

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const entryPath = path.join(__dirname, '/client/src'); 
+const outputPath = path.join(__dirname, '/client/dist');
+
+
+module.exports = {
+  entry: `${entryPath}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: `${outputPath}`,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
