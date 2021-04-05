41d139965ec54771c8b07b7294ec1e3deaf5d075
David Palaciose <dboxing22@gmail.com>
2021-04-05 16:34:25 -0500

    figured out how to use react router with styled components. Now the navbar looks better and getting close to the team goal

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,11 +1,96 @@
-import React, { Component } from 'react';
-class About extends Component{
-    render(){
-        return(
-            <div>
-                <h1>Hello About Page</h1>
-            </div>
-        );
+import React, { useState } from 'react';
+import styled from 'styled-components';
+const BarStyle = styled.div`
+  background: rgb(35, 35, 35);
+  color: rgb(240, 240, 240);
+  display: flex;
+  flex-flow: column;
+  padding: 20%;
+  /* align-items: center; */
+  /* margin-top: auto; */
+  width: 490px;
+  button, input{
+    background: rgb(35, 35, 35);
+    color: rgb(240, 240, 240);
+    font-size: 14px;
+    width: fit-content;
+    padding: 0.25rem 0.75rem;
+    border: none;
+    outline: none;
+    border-radius: 0.25rem;
+    box-shadow: -1px -1px 2px rgba(100,100,100, 1), 1px 1px 1px rgba(0,0,0, 1);
+    :active{
+      box-shadow: -1px -1px 1px rgba(100,100,100, 1), 1px 1px 1px rgba(0,0,0, 1);
+      background: rgb(25, 25, 25);
     }
-}
-export default About;
+    :hover{
+      box-shadow: -2px -2px 6px rgba(100,100,100, 1), 3px 3px 4px rgba(0,0,0, 1);
+    }
+  };
+  h3{
+    color: cornflowerblue
+  };
+  *{
+    flex-flow: column;
+    align-items: center;
+  }
+`;
+// the bar component will have a search form where users enter ingredients, and a button to add them to that user's bar
+// when a user enters ingredient and clicks 'add' button, ingredient will be added to user's bar in database
+const Bar = () => {
+  const [ ingredient, setIngredient ] = useState('');
+  const [ ingredientsList, setIngredientsList ] = useState([]);
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setIngredient(value);
+  };
+  const handleClick = async () => {
+    setIngredientsList((prevList) => {
+      return [ ...prevList, ingredient ];
+    });
+    await setIngredient('');
+  };
+  const handleKeyDown = (e) => {
+    const { key } = e;
+    key === 'Enter' ?
+      handleClick() :
+      null;
+  };
+  const ingredientsListMap = ingredientsList.map(ingredient => {
+    return (
+      <li key={1}>
+        {ingredient}
+      </li>
+    );
+  });
+  return (
+    <BarStyle>
+      <div>
+        <input
+          type='text'
+          name='ingredient'
+          value={ingredient}
+          placeholder='Enter ingredients...'
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        >
+        </input>
+        <button
+          onClick={handleClick}
+        >
+          Add to Bar
+        </button>
+        <br />
+        <br />
+        <br />
+        <div>
+          My Bar:
+          <ul>
+            {ingredientsListMap}
+          </ul>
+        </div>
+      </div>
+    </BarStyle>
+  );
+};
+export default Bar;
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,37 +1,148 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, Form, FormControl, Button, NavItem } from 'react-bootstrap';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Navbar,  Form, FormControl, Button, NavItem } from 'react-bootstrap';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import About from './About'
+import Bar from './About.jsx'
 
+
+const Nav = styled.nav`
+  padding: 0 20px;
+  min-height: 9vh;
+  background: #1c2022;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.h1`
+  font-size: 25px;
+  color: white;
+`;
+
+const Menu = styled.ul`
+  list-style: none;
+  display: flex;
+  li:nth-child(2) {
+    margin: 0px 20px;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Item = styled.li``;
+
+
+
+const NavIcon = styled.button`
+  background: none;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
+
+const Line = styled.span`
+  display: block;
+  border-radius: 50px;
+  width: 25px;
+  height: 3px;
+  margin: 5px;
+  background-color: #fff;
+  transition: width 0.4s ease-in-out;
+  :nth-child(2) {
+    width: ${props => (props.open ? "40%" : "70%")};
+  }
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  height: ${props => (props.open ? "91vh" : 0)};
+  width: 100vw;
+  background: #1c2022;
+  transition: height 0.4s ease-in-out;
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
+
+const OverlayMenu = styled.ul`
+  list-style: none;
+  position: absolute;
+  left: 50%;
+  top: 45%;
+  transform: translate(-50%, -50%);
+  li {
+    opacity: ${props => (props.open ? 1 : 0)};
+    font-size: 25px;
+    margin: 50px 0px;
+    transition: opacity 0.4s ease-in-out;
+  }
+  li:nth-child(2) {
+    margin: 50px 0px;
+  }
+`;
 
 //using react bootstrap to create the navbar and using react router to create a route for each component we will use
 //This is a dummy version, in which we will eventually add the main components we need to route
 //Main Goal: each tab will conditionally render a certain component to display depending on the route
 const MyNavbar = () => {
-
+  const [toggle, toggleNav] = useState(false);
   return (
     <div>
     <div>
-      <Navbar>
-        <Navbar.Brand as={Link} to="/" >Shaker</Navbar.Brand>
-        <Navbar.Collapse>
-          <Nav className="mr-auto">
-            <NavItem eventkey={1} href="/">
-              <Nav.Link as={Link} to="/about" >Home</Nav.Link>
-            </NavItem>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
+    <Nav>
+        <Logo>Shaker</Logo>
+        <Menu>
+          <Item>
+            <Link as={Link} to="/about" >
+              Search
+            </Link>
+          </Item>
+          <Item>
+            <Link target="#" href="https://en.wikipedia.org/wiki/Vodka">
+              Vodka
+            </Link>
+          </Item>
+          <Item>
+            <Link target="#" href="https://flordecana.com/">
+              Flor De Cana
+            </Link>
+          </Item>
+        </Menu>
+        <NavIcon onClick={() => toggleNav(!toggle)}>
+          <Line open={toggle} />
+          <Line open={toggle} />
+          <Line open={toggle} />
+        </NavIcon>
+      </Nav>
+      <Overlay open={toggle}>
+        <OverlayMenu open={toggle}>
+          <Item>
+            <Link target="#" href="https://www.google.com/">
+            Vodka
+            </Link>
+          </Item>
+          <Item>
+            <Link target="#"  href="https://en.wikipedia.org/wiki/Vodka">
+              Vodka
+            </Link>
+          </Item>
+          <Item>
+            <Link target="#" href="https://flordecana.com/">
+              Flor De Cana
+            </Link>
+          </Item>
+        </OverlayMenu>
+      </Overlay>
     </div>
     <div>
       <Switch>
-        <Route exact path='/about' component={About} />
+        <Route exact path='/about' component={Bar} />
         <Route render={function () {
-          return <p>Not found</p>
+          return <p>Hey!!!!</p>
         }} />
       </Switch>
     </div>
@@ -40,3 +151,22 @@ const MyNavbar = () => {
 };
 
 export default MyNavbar
+
+
+
+
+{/* <Navbar>
+<Navbar.Brand as={Link} to="/" >Shaker</Navbar.Brand>
+<Navbar.Collapse>
+  <Nav className="mr-auto">
+    <NavItem eventkey={1} href="/">
+      <Nav.Link as={Link} to="/about" >Home</Nav.Link>
+      <Nav.Link as={Link} to="/about" >Bar</Nav.Link>
+    </NavItem>
+  </Nav>
+  <Form inline>
+    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+    <Button variant="outline-success">Search</Button>
+  </Form>
+</Navbar.Collapse>
+</Navbar> */}
