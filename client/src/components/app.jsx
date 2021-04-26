483abc01302b28ab38fc62862c39aa5ffbc21673
Christopher Golizio <cmgolizio@gmail.com>
2021-04-26 10:49:36 -0500

    trying to deploy

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,4 +1,4 @@
-import NavBar from './Navbar.jsx';
+import NavBar from './navbar.jsx';
 import styled from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 import React, { Component } from 'react';
